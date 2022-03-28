fa4cf30308e24ca7b48d56a0c8d4e6d7a2b16293
Jai Kumar <kjai1903@gmail.com>
2022-03-28 01:56:32 -0400

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Lab07_101236674';
+  title = 'Lab 07 - COMP 3133 (W2022)';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RemoveSpaces } from './remove-spaces.pipe';
+import { InputFormatDirective } from './input-format.directive';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroessComponent
+    HeroesComponent,
+    RemoveSpaces,
+    InputFormatDirective,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
